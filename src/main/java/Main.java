052202a916b3771c9cab7183f480cdd04b3afe30
Nigel <nigel@Zipcoders-MacBook-Pro-39.local>
2023-06-29 17:38:35 -0400

    Finish Project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
-public class Main {
 
-    public static void main(String[] args){
-        
+import java.util.Scanner;
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Guess the Number!");
+        int n = 20;
+        int guess = 0;
+        int counter = 0;
+
+        while (guess != n) {
+            System.out.println("You Guessed:");
+            guess = in.nextInt();
+            counter++;
+
+            if (guess > n) {
+                System.out.println("Too Large");
+            } else if (guess < n) {
+                System.out.println("Too Large");
+            } else {
+                System.out.println("Your Correct!");
+                System.out.println("It took you " + counter + " times to guess correctly");
+            }
+        }
     }
 }
